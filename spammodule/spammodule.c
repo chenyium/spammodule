389a1f103bcb6708ecc30c33c01cfb9ea4cfa819
chenyao <cheny@meizu.com>
2016-04-17 20:48:56 +0800

    add callback

--- spammodule/spammodule.c
+++ spammodule/spammodule.c
@@ -23,8 +23,47 @@ static PyObject* spam_system(PyObject *self, PyObject *args)
 	return Py_BuildValue("i", ret);
 }
 
+static PyObject* my_callback = NULL;
+static PyObject* callback_set(PyObject *self, PyObject *args)
+{
+	PyObject *temp;
+	if (!PyArg_ParseTuple(args, "O:set_callback", &temp))
+		return NULL;
+
+	if (!PyCallable_Check(temp)) {
+		PyErr_SetString(PyExc_TypeError, "parameter must be callable");
+		return NULL;
+	}
+
+	Py_XINCREF(temp);         /* Add a reference to new callback */
+	Py_XDECREF(my_callback);  /* Dispose of previous callback */
+	my_callback = temp;       /* Remember new callback */
+
+	/* Boilerplate to return "None" */
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+static PyObject* callback_call(PyObject *self, PyObject *args)
+{
+	PyObject *result;
+	if (NULL == my_callback) {
+		PyErr_SetString(SpamNotFound, "callback is null");
+		return NULL;
+	}
+
+	/* Time to call the callback */
+	result = PyObject_CallObject(my_callback, args);
+	if (result == NULL)
+		return NULL; /* Pass error back */
+
+	return result;
+}
+
 static PyMethodDef SpamMethods[] = {
 	{ "system", spam_system, METH_VARARGS, "Execute a shell command" },
+	{ "callbackset", callback_set, METH_VARARGS, "Set a callback function" },
+	{ "callbackcall", callback_call, METH_VARARGS, "Call a callback function" },
 	{ NULL, NULL, 0, NULL }
 };
 
